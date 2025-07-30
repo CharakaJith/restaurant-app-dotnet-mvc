adb31ee814603f14a35d0e157aa8494016450c60
Charaka Gunasinghe <charaka.info@gmail.com>
2025-07-30 18:44:27 +0530

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace restaurant_app_dotnet_mvc.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
