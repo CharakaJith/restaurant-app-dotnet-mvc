97f7f5242ab86075d6e5d62cd173f7b6f6430af8
Charaka Gunasinghe <charaka.info@gmail.com>
2025-08-02 22:58:08 +0530

    repository logic implemented

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -15,14 +15,17 @@ namespace restaurant_app_dotnet_mvc.Models
             _dbSet = context.Set<T>();
         }
 
-        public Task AddAsync(T entity)
+        public async Task AddAsync(T entity)
         {
-            throw new NotImplementedException();
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
         }
 
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            T entity = await _dbSet.FindAsync(id);
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<IEnumerable<T>> GetAllAsync()
@@ -30,14 +33,35 @@ namespace restaurant_app_dotnet_mvc.Models
             return await _dbSet.ToListAsync();
         }
 
-        public Task<T> GetByIdAsync(int id, QueryOption<T> options)
+        public async Task<T> GetByIdAsync(int id, QueryOption<T> options)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = _dbSet;
+            
+            if (options.HasWhere)
+            {
+                query = query.Where(options.Where);
+            }
+            if (options.HasOrderBy)
+            {
+                query = query.OrderBy(options.OrderBy);
+            }
+
+            foreach (string includes in options.GetIncludes())
+            {
+                query = query.Include(includes);
+            }
+
+            var key = _context.Model.FindEntityType(typeof(T)).FindPrimaryKey().Properties.FirstOrDefault();
+            string pkName = key?.Name;
+
+            return await query.FirstOrDefaultAsync(e => EF.Property<int>(e, pkName) == id);
         }
 
-        public Task UpdateAsync(T entity)
+        public async Task UpdateAsync(T entity)
         {
-            throw new NotImplementedException();
+            _context.Update(entity);
+
+            await _context.SaveChangesAsync();
         }
     }
 }
